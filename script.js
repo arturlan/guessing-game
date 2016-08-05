6f023bf58337c01d74a8b9d3bed6a2f63b8d40f8
Artur Lane <arturlan.us@gmail.com>
2016-08-05 15:07:54 -0400

    simple implementation added

---
+++ script.js
@@ -0,0 +1,35 @@
+var targetNumber
+var attempts = 0
+var finished = false
+var guessInputInt
+
+function startGame() {
+  var randomNumber = Math.floor((Math.random() * 100) + 1)
+  targetNumber = randomNumber
+
+  while (!finished) {
+    var guessInput = prompt("Let's play a game 1 to 100 \n\n" +
+    "Enter your number")
+
+    guessInputInt = parseInt(guessInput)
+    finished = checkGuess();
+    attempts++
+  }
+
+}
+
+function checkGuess() {
+  if (guessInputInt == null) {
+    alert("please enter a integer")
+    return fasle
+  } else if (guessInputInt > targetNumber) {
+    alert("your number is too big")
+    return false
+  } else if (guessInputInt < targetNumber) {
+    alert("your number is too small")
+    return false
+  } else if (guessInputInt == targetNumber) {
+    alert("you win, congratulation \n\n" + attempts + " attemps took to guess number" )
+    return true
+  }
+}
